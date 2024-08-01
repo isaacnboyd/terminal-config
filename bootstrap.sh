b568acd0891496f88aed1f04ddebed7d44d93160
🦅 Isaac Boyd <isaacnboyd@gmail.com>
2024-08-01 11:58:30 +0000

    Adding info to install minikube a and google cloud cli tool
    Signed-off-by: 🦅 Isaac Boyd <isaacnboyd@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update
+
 sudo snap install glow
 
 sudo snap install go --classic 
@@ -7,3 +9,26 @@ sudo snap install go --classic
 vim -E -c GoInstallBinaries -c q
 
 vim -E -c helptags ALL -c q
+
+sudo snap install docker
+
+sudo chown $USER /var/run/docker.sock
+
+minikube start
+
+cd /tmp
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+sudo apt-get install apt-transport-https ca-certificates gnupg curl
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+sudo apt-get update && sudo apt-get install google-cloud-cli
+
+gcloud init
+
+cd ~
